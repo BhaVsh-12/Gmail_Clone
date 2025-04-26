08d8aeed865d0b8c8264cf4a54b7f996cc7d9955
bhave <bhaveshbari0402@gmail.com>
2025-04-26 19:47:52 +0530

    backend

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URI;
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URI || "http://localhost:8080";
 
 const Api = axios.create({
   baseURL: API_BASE_URL,
